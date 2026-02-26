4a506d7af1fe3c1eba1d689a75be8554154cbac2
postershub74-sys <postershub74@gmail.com>
2026-02-27 01:01:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@ import { motion } from "framer-motion";
 export default function App() {
   const [search, setSearch] = useState("");
 
- const products = [
-  { id: 1, name: "hot wheels 1985 Honda CR-X 1:64", price: 1999, image: "/images/gtr.jpg" },
-  { id: 2, name: "Toyota Supra MK4 1:24", price: 3499, image: "/images/supra.jpg" },
-  { id: 3, name: "Lamborghini Poster A3", price: 399, image: "/images/poster.jpg" },
-  { id: 4, name: "Custom Car Poster", price: 699, image: "/images/custom.jpg" }
-];
-];
+  const products = [
+    { id: 1, name: "hot wheels 1985 honda CR-X 1:64", price: 1999, image: "/images/gtr.jpg" },
+    { id: 2, name: "Toyota Supra MK4 1:24", price: 3499, image: "/images/supra.jpg" },
+    { id: 3, name: "Lamborghini Poster A3", price: 399, image: "/images/poster.jpg" },
+    { id: 4, name: "Custom Car Poster", price: 699, image: "/images/custom.jpg" }
+  ];
 
   const filtered = products.filter((p) =>
     p.name.toLowerCase().includes(search.toLowerCase())
@@ -44,19 +43,21 @@ export default function App() {
               borderRadius: "15px"
             }}
           >
-           <img 
-  src={product.image} 
-  alt={product.name}
-  style={{
-    height: "150px",
-    width: "100%",
-    objectFit: "cover",
-    borderRadius: "10px",
-    marginBottom: "15px"
-  }}
-/>
+            <img 
+              src={product.image} 
+              alt={product.name}
+              style={{
+                height: "180px",
+                width: "100%",
+                objectFit: "cover",
+                borderRadius: "10px",
+                marginBottom: "15px"
+              }}
+            />
+
             <h2>{product.name}</h2>
             <p style={{ fontSize: "20px" }}>₹{product.price}</p>
+
             <button
               style={{
                 padding: "10px",
